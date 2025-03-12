5ea49b1e74a2979f81fb81f2d67bacb5813058d4
Andreas Toledo <eduardo.araujo.toledo1993@gmail.com>
2025-03-12 18:25:28 -0300

    Menu Buttons

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,9 @@
     <body>
     <h1>Bem-vindo ao CRUD</h1>
     <ul>
-        <li><a href="{{ route('users.index') }}">Gerenciar Usuários</a></li>
-        <li><a href="{{ route('books.index') }}">Gerenciar Livros</a></li>
-        <li><a href="{{ route('products.index') }}">Gerenciar Produtos</a></li>
+        <li><a href="{{ route('users.index') }}" style="display: inline-block; padding: 10px 15px; background-color: red; color: white; text-decoration: none; border-radius: 5px; cursor: pointer; margin: 15px;">Gerenciar Usuários</a></li>
+        <li><a href="{{ route('books.index') }}" style="display: inline-block; padding: 10px 15px; background-color: blue; color: white; text-decoration: none; border-radius: 5px; cursor: pointer; margin: 15px;">Gerenciar Livros</a></li>
+        <li><a href="{{ route('products.index') }}" style="display: inline-block; padding: 10px 15px; background-color: purple; color: white; text-decoration: none; border-radius: 5px; cursor: pointer; margin: 15px;">Gerenciar Produtos</a></li>
     
     </body>
 </html>
